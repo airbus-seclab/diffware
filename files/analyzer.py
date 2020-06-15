5cb2e251a99fbf40af92142872c49369e0cc97f7
Jean-Romain Garnier github@jean-romain.com <github@jean-romain.com>
2020-06-15 15:17:56 +0200

    Refactor ELF analysis
    Make code cleaner and more reusable by creating generic classes in analyzer.py and refactoring elf.py

---
+++ files/analyzer.py
@@ -0,0 +1,93 @@
+import re
+import subprocess
+
+from profiler import Profiler
+from logger import Logger
+
+
+class Analyzer:
+    def __init__(self, path):
+        self.path = path
+
+    def run(self):
+        """
+        Builds a generator with the filtered output lines
+        of all commands that this Analyzer runs
+        """
+        raise NotImplementedError
+
+
+class Regex:
+    def __init__(self, pattern, replace):
+        self.pattern = pattern
+        self.replace = replace
+
+    @property
+    def pattern(self):
+        return self._compiled.pattern
+
+    @pattern.setter
+    def pattern(self, pattern):
+        self._compiled = re.compile(pattern)
+
+    def apply(self, string):
+        """
+        Returns the string with the substrings matching self.pattern replaced
+        by self.replace
+        """
+        return self._compiled.sub(self.replace, string)
+
+
+class Command:
+    @classmethod
+    def make_regex(self, path):
+        """
+        Returns a list of Regex instances to apply to the output of this command
+        """
+        return []
+
+    @classmethod
+    def cmd_options(self):
+        """
+        Returns a list of options needed for this command
+        """
+        return []
+
+    @classmethod
+    def make_cmd(self, file):
+        """
+        Returns a list, as accepted by subprocess.run, of strings to run
+        this command for the given file
+        """
+        raise NotImplementedError
+
+    @classmethod
+    @Profiler.profilable
+    def run(self, sections, file):
+        """
+        Run this command and return a generator of filtered output lines
+        """
+        regex = self.make_regex(file)
+        cmd = self.make_cmd(sections, file)
+
+        # Handle empty commands to save some time
+        if cmd is None:
+            return iter(())
+
+        Logger.debug("Running command {}".format(" ".join(cmd)))
+        process = subprocess.run(
+            cmd,
+            shell=False,
+            close_fds=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL
+        )
+
+        return self._filter(process.stdout.splitlines(True), regex)
+
+    @classmethod
+    def _filter(self, output, regex):
+        for line in output:
+            for reg in regex:
+                line = reg.apply(line)
+            yield line
--- files/elf.py
+++ files/elf.py
@@ -1,6 +1,7 @@
 import os
 import re
 import tlsh
+import itertools
 import subprocess
 import collections
 
@@ -9,111 +10,106 @@ from utils import cached_property
 from profiler import Profiler
 from logger import Logger
 from file_comparator import FileComparator
+from files.analyzer import Analyzer, Command, Regex
 
 from fact_helper_file import get_file_type_from_path
 
 
-class ElfSection:
-    def __init__(self, path, section_name):
-        self.name = section_name
-        self.path = path
+class ElfAnalyzer(Analyzer):
+    def __init__(self, path):
+        super().__init__(path)
+        self.code_sections = []
+        self.data_sections = []
 
+    def is_empty(self):
+        return not self.code_sections and not self.data_sections
+
+    def add_section(self, section_name, flags):
+        if "X" in flags:
+            self.code_sections.append(section_name)
+        else:
+            self.data_sections.append(section_name)
+
+    def run(self):
+        section_output = ElfSectionCommand.run(self.data_sections, self.path)
+        code_output = ElfCodeSectionCommand.run(self.code_sections, self.path)
+        return itertools.chain(section_output, code_output)
+
+
+class ElfSectionCommand(Command):
     @classmethod
-    def _init_regex(self, path):
+    def make_regex(self, path):
         path_dir = re.escape(os.path.dirname(path).encode("utf-8"))
         path = re.escape(path.encode("utf-8"))
+        regex_list = []
 
-        # Match the directory containing the file
-        self._dir_re = re.compile(
-            rb"%s\b" % path_dir
-        )
-        # Match the full path to the file
-        self._path_re = re.compile(rb"%s\b" % path)
-        # Match hex offset
-        self._offset_re = re.compile(rb"offset 0x[0-9a-f]+")
+        # Order matters:
+        # First match the full path to the file
+        regex_list.append(Regex(rb"%s\b" % path, b"/file"))
+
+        # Then match the directory containing the file
+        regex_list.append(Regex(rb"%s\b" % path_dir, b"/"))
+
+        # Finally, match every hex value (possible offset or address)
+        regex_list.append(Regex(rb"0x[0-9a-f]+", b"0x"))
+
+        return regex_list
 
     @classmethod
-    def _cmd_options(self):
+    def cmd_options(self):
         return ["--decompress"]
 
     @classmethod
-    def _cmd(self, sections, file):
-        cmd = self._cmd_options()
+    def make_cmd(self, sections, file):
+        # Don't run a command if there are no sections
+        if not sections:
+            return
+
+        cmd = self.cmd_options()
 
         # Join all section names together to dump all at once
         for section in sections:
-            cmd += ["--hex-dump", section.name]
+            cmd += ["--hex-dump", section]
 
         return ["readelf", *cmd, file]
 
+
+class ElfCodeSectionCommand(Command):
     @classmethod
-    @Profiler.profilable
-    def analyze(self, sections, file):
-        self._init_regex(file)
-        cmd = self._cmd(sections, file)
-
-        Logger.debug("Running command {}".format(" ".join(cmd)))
-        self._process = subprocess.run(
-            cmd,
-            shell=False,
-            close_fds=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL
-        )
-
-    @classmethod
-    def _filter(self, line):
-        # The full path can appear in the output, we need to remove it
-        line = self._path_re.sub(b"/file", line)
-        line = self._dir_re.sub(b"/", line)
-        # Offsets can change if headers are modified, but don't reflect
-        # real modifications in behavior
-        return self._offset_re.sub(b"offset 0x", line)
-
-    @classmethod
-    def _read(self):
-        for line in self._process.stdout.splitlines(True):
-            yield self._filter(line)
-
-
-class ElfCodeSection(ElfSection):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def make_regex(self, path):
+        regex_list = []
 
         # Match the leading hex value (offset of the instruction)
-        self._line_regex = re.compile(rb'^\s*[0-9a-f]+:\s*')
+        regex_list.append(Regex(rb"^\s*[0-9a-f]+:\s*", b""))
+
         # Match addresses that have been resolved to a symbol
         # First, parse lines that look like:
         # "​callq 4f60 <call_gmon_start>"
-        self._resolved_regex = re.compile(rb'\b([0-9a-f]+)(?=\s+\<\S+\>)')
+        regex_list.append(Regex(rb"\b([0-9a-f]+)(?=\s+\<\S+\>)", b""))
+
         # Then, parse lines that look like:
         # "lea  0x111cb4 (%rip),%rsi     # 12a012 <_fini+0xc4>"
-        self._pointer_regex = re.compile(rb'\b(0x[0-9a-f]+)\b(?=.*\<\S+\>)')
+        regex_list.append(Regex(rb"\b(0x[0-9a-f]+)\b(?=.*\<\S+\>)", b"0x"))
+
+        return regex_list
 
     @classmethod
-    def _init_regex(self, path):
-        pass
-
-    @classmethod
-    def _cmd_options(self):
+    def cmd_options(self):
         return ["--disassemble", "--demangle", "--reloc", "--no-show-raw-insn"]
 
     @classmethod
-    def _cmd(self, sections, file):
-        cmd = self._cmd_options()
+    def make_cmd(self, sections, file):
+        # Don't run a command if there are no sections
+        if not sections:
+            return
+
+        cmd = self.cmd_options()
 
         # Join all section names together to dump all at once
         for section in sections:
-            cmd += ["-j", section.name]
+            cmd += ["-j", section]
 
         return ["readelf", *cmd, file]
-
-    @classmethod
-    def _filter(self, line):
-        line = super()._filter(line)
-        line = self._line_regex.sub(b"", line)
-        line = self._resolved_regex.sub(b"", line)
-        return self._pointer_regex.sub(b"0x ", line)
 
 
 class ElfFile(UnpackedFile):
@@ -137,12 +133,6 @@ class ElfFile(UnpackedFile):
         "<corrupt>"
     ]
 
-    SECTION_FLAG_MAPPING = {
-        "X": ElfCodeSection,
-        "S": ElfSection,
-        "_": ElfSection
-    }
-
     @cached_property
     def _comparable_path(self):
         # Use readelf and objdump to extract the useful info and write it to
@@ -155,19 +145,12 @@ class ElfFile(UnpackedFile):
         self._load_sections(absolute_path)
 
         # If there were no sections, default to comparing the whole file
-        if not self._code_sections and not self._data_sections:
+        if self._analyzer.is_empty():
             return self.path
 
         with open(tmp_file_path, "wb") as tmp:
-            if self._data_sections:
-                ElfSection.analyze(self._data_sections, absolute_path)
-                for line in ElfSection._read():
-                    tmp.write(line)
-
-            if self._code_sections:
-                ElfCodeSection.analyze(self._code_sections, absolute_path)
-                for line in ElfCodeSection._read():
-                    tmp.write(line)
+            for line in self._analyzer.run():
+                tmp.write(line)
 
         return tmp_file_path
 
@@ -179,6 +162,9 @@ class ElfFile(UnpackedFile):
         """
         Inspired by diffoscope/comparator/elf.py ElfContainer
         """
+        self._analyzer = ElfAnalyzer(file)
+
+        # Get all the sections in this file to pass them to the analyzer
         cmd = [
             "readelf",
             "--wide",
@@ -197,30 +183,18 @@ class ElfFile(UnpackedFile):
 
             # Entries of readelf --section-headers have the following columns:
             # [Nr]  Name  Type  Address  Off  Size  ES  Flg  Lk  Inf  Al
-            self._sections = collections.OrderedDict()
-
             for line in output:
                 if line.startswith("Key to Flags"):
                     break
 
                 # Strip number column because there may be spaces in the brakets
                 line = line.split("]", 1)[1].split()
-                name, type, flags = line[0], line[1], line[6] + "_"
+                name, type, flags = line[0], line[1], line[6]
 
                 if self._should_skip_section(name, type):
                     continue
 
-                # Use first match, with last option being "_" as fallback
-                elf_class = [
-                    self.SECTION_FLAG_MAPPING[x]
-                    for x in flags
-                    if x in self.SECTION_FLAG_MAPPING
-                ][0]
-
-                if issubclass(elf_class, ElfCodeSection):
-                    self._code_sections.append(elf_class(file, name))
-                else:
-                    self._data_sections.append(elf_class(file, name))
+                self._analyzer.add_section(name, flags)
         except Exception as e:
             Logger.error("error in _load_sections", e)
             pass
