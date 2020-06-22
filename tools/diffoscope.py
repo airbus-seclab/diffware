fba6923bbbf5739c8dcf35720679d2264e1330ce
Jean-Romain Garnier <github@jean-romain.com>
2020-06-22 17:00:51 +0200

    Fix call to diffoscope external dependency

--- tools/diffoscope.py
+++ tools/diffoscope.py
@@ -126,7 +126,7 @@ def copy_files(pairs, tmp1, tmp2):
 def call_diffoscope(dir1, dir2, args):
     # Set max-container-depth to 0 so diffoscope doesn't try to extract files
     # which we know have already been extracted
-    cmd = ["python3", DIFFOSCOPE_PATH, dir1.name, dir2.name, "--max-container-depth", "0", *args]
+    cmd = [DIFFOSCOPE_PATH, dir1.name, dir2.name, "--max-container-depth", "0", *args]
     subprocess.run(cmd)
 
 
